97530ab636138699a81ecd3b3a65d2f25f33ea1b
mpario <79463549+mpario@users.noreply.github.com>
2021-04-05 06:42:46 -0700

    file path created and vars assigned.

---
+++ PyPoll/main.py
@@ -0,0 +1,41 @@
+import os
+import csv
+
+electiondata_path = os.path.join('Resources', 'election_data.csv')
+
+#empty list to store data
+voter_id = []
+county = []
+candidate = []
+total_votes = []
+
+prof_change = []
+average_change = []
+inc_profits = []
+dec_profits = []
+
+
+#open and read csv file
+with open(electiondata_path) as electiondata_handler:
+	#when a command is made, electiondata_object will pass it in a list (separated by commas)
+	electiondata_object = csv.reader(electiondata_handler, delimiter=',')
+	electiondata_header = next(electiondata_object)
+	#print(electiondata_header)
+
+	for row in electiondata_object:
+		#Total Months
+		voter_id.append(row[0])
+		total_votes = len(voter_id)
+		#Total
+		prof_row = prof_loss.append(int(row[1]))
+		net_total = sum(prof_loss)
+
+
+print(f"Election Results")
+print(f"--------------------------------")
+print(f"Total Votes: {total_month_count}")
+print(f"--------------------------------")
+print(f"Khan: {net_total}")
+print(f"Correy: {average_change}")
+print(f"Li: ")
+print(f"O'Tooley: ")
