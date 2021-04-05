f7ed015a36bbd89d57159af8b64da0a13f3c917b
mpario <79463549+mpario@users.noreply.github.com>
2021-04-05 12:16:43 -0700

    still working on candidate count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,12 +7,21 @@ electiondata_path = os.path.join('Resources', 'election_data.csv')
 voter_id = []
 county = []
 candidate = []
-total_votes = []
+#total_votes = []
 
 prof_change = []
 average_change = []
 inc_profits = []
 dec_profits = []
+
+khan_count = 0
+correy_count = 0
+li_count = 0
+otooley_count = 0
+khan_vote = 0
+correy_vote = 0
+li_vote = 0
+otooley_vote = 0
 
 
 #open and read csv file
@@ -23,19 +32,33 @@ with open(electiondata_path) as electiondata_handler:
 	#print(electiondata_header)
 
 	for row in electiondata_object:
-		#Total Votes
+		# Total Votes
 		voter_id.append(row[0])
 		total_votes = len(voter_id)
-		#Total
-		# prof_row = prof_loss.append(int(row[1]))
-		# net_total = sum(prof_loss)
+		
+		# Per Candidate
 
+		candidate.append([row[2]])
+		if (candidate == "Khan"):
+			khan_count += 1
+			khan_vote = len(khan_count)
+		elif (candidate == "Correy"):
+			correy_count += 1
+			correy_vote = len(correy_count)
+		elif (candidate == "Li"):
+			li_count += 1
+			li_vote = len(li_count)
+		elif (candidate == "O'Tooley"):
+			otooley_count += 1
+			otooley_vote = len(otooley_count)
+
+# print(candidate.append([row[2]]))
 
 print(f"Election Results")
 print(f"--------------------------------")
 print(f"Total Votes: {total_votes}")
 print(f"--------------------------------")
-print(f"Khan: ")
-print(f"Correy: ")
-print(f"Li: ")
-print(f"O'Tooley: ")
+print(f"Khan: {khan_vote}")
+print(f"Correy: {correy_vote}")
+print(f"Li: {li_vote}")
+print(f"O'Tooley: {otooley_vote}")
